5e1bc79f661dd8e90b4b7d67c8bdae0b8f377d33
UmangApexon <umang.bheda@apexon.com>
2023-11-20 13:03:30 +0530

    component added

---
+++ src/app/contacts/profile-details-shimmer/profile-details-shimmer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-details-shimmer',
+  templateUrl: './profile-details-shimmer.component.html',
+  styleUrls: ['./profile-details-shimmer.component.scss']
+})
+export class ProfileDetailsShimmerComponent {
+
+}
